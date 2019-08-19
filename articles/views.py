fb3cda2b9a765a6bed877415257bcadd6ed9ac49
EHwooKim <ehwoo0707@naver.com>
2019-08-19 15:43:14 +0900

    03 | Articles Index

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
+
+def index(request):
+    articles = Article.objects.all() # 작성
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
